41e664c00bebb5f7f9bd62b0115b200ed28fc143
sudhirgudapati1998 <51699456+sudhirgudapati1998@users.noreply.github.com>
2019-07-16 14:54:13 +0530

    Create repeat1.py

---
+++ repeat1.py
@@ -0,0 +1,17 @@
+n=int(input())
+k=0
+lst=input()
+lst=lst.split()
+lst=list(map(int,lst))
+new=[]
+for i in range(0,n):
+    if((lst.count(lst[i]))>=2):
+      if lst[i] not in new:
+        new.append(lst[i])
+        k=1
+if(k==0):
+  print("unique")
+else:
+  for i in range(0,n):
+    print(min(new),end=" ")
+    new.remove(min(new))
